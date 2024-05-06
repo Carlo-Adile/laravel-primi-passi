21a188abd1122faf4ad46562ab326ca44f13a239
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-06 16:27:34 +0200

    refactor navbar in a different view file and include in other views

---
+++ laravel-primi-passi/resources/views/includes/navbar.blade.php
@@ -0,0 +1,20 @@
+<nav id="my_navbar">
+      <div class="container-xl debug">
+        <div class="row">
+          <!-- logo -->
+          <div>
+            <a href="" id="nav_logo">
+              <img src="{{ asset('img/logomark.min.svg') }}" alt="">
+              <img src="{{ asset('img/logotype.min.svg') }}" alt="">
+            </a>
+          </div>
+          <!-- nav menu -->
+          <div class="debug">
+            <ul>
+              <li><a href="/">HOME</a></li>
+              <li><a href="/about">ABOUT</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
